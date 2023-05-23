c47d2293aae02f739e6473e976ec7d43251db733
domenicolorenti <domenicolorenti.dev@gmail.com>
2023-05-23 23:28:11 +0200

    fix: login fix end

--- profilePBC/src/main/java/org/itinera/controller/AuthController.java
+++ profilePBC/src/main/java/org/itinera/controller/AuthController.java
@@ -4,12 +4,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.itinera.controller.comunication.Credentials;
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @PostMapping("/login")
